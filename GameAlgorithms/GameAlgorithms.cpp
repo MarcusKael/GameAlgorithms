46a5812e9e8faac79ce17fa13ba25a2cb1ddfd9d
Franco Báez Farías <f.baezfarias@hotmail.com>
2020-11-25 10:44:07 -0300

    Variable size matrix

--- GameAlgorithms/GameAlgorithms.cpp
+++ GameAlgorithms/GameAlgorithms.cpp
@@ -10,8 +10,9 @@
 
 using namespace std;
 
-void printArray(vector<int> arr, int n) {
-    int i;
+void printArray(vector<int> arr, size_t n) {
+    // Esto imprime un array entregado
+    size_t i;
     cout << " ";
     for (i = 0; i < n; i++) {
         cout << arr[i] << " ";
@@ -21,6 +22,7 @@ void printArray(vector<int> arr, int n) {
 
 void remove(vector<int> &v)
 {
+    // Esto elimina duplicados
     auto end = v.end();
     for (auto it = v.begin(); it != end; ++it) {
         end = std::remove(it + 1, end, *it);
@@ -30,29 +32,23 @@ void remove(vector<int> &v)
 }
 
 vector<int> insertionSort(vector<int> arr) {
-    size_t i;
-    int key, j;
-    // cout << "\n Array size: " << arr.size() << endl;
+    size_t i, j;
+    int key;
     for (i = 1; i < arr.size(); i++) {
-
-        key = arr[i];
-
-        j = i - 1;
-
-        while (j >= 0 && key < arr[j]) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        key = arr[i];//take value
+        j = i;
+        while (j > 0 && arr[j - 1] > key) {
+            arr[j] = arr[j - 1];
+            j--;
         }
-        arr[j + 1] = key;
+        arr[j] = key;   //insert in right place
     }
-
-    // cout << "Printing array: " << endl;
-    // printArray(arr, arr.size());
-
     return arr;
 }
 
-int chebyshevDistance(int maze[10][10], int x1, int y1, int x2, int y2) {
+int chebyshevDistance(int** maze, int x1, int y1, int x2, int y2) {
+    // Esto calcula la distancia chebyshev
+    // cout << "\n Chebycheck: " << maze[x1][y1] << " ";
     if (maze[x1][y1] > 1) {
         return 100000;
     }
@@ -61,10 +57,12 @@ int chebyshevDistance(int maze[10][10], int x1, int y1, int x2, int y2) {
     return (dx + dy) - min(dx, dy);
 }
 
-void solLen(int sol[10][10]) {
-    int counter = 0, i, j;
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+void solLen(int** sol, size_t size) {
+    // Esto imprime por pantalla el tamaño de la solúción
+    int counter = 0;
+    size_t i, j;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             if (sol[i][j] == 1) {
                 counter++;
             }
@@ -74,32 +72,35 @@ void solLen(int sol[10][10]) {
     cout << "\n Size of solution: " << counter;
 }
 
-void printSolution(int sol[10][10]) {
-    int i, j;
-    for (i = 0; i < 10; i++) {
+void printSolution(int** sol, size_t size) {
+    // Esto imprime por pantaña la solución
+    size_t i, j;
+    for (i = 0; i < size; i++) {
         cout << " ";
-        for (j = 0; j < 10; j++) {
+        for (j = 0; j < size; j++) {
             cout << sol[i][j];
             cout << " ";
         }
         cout << "\n";
     }
 
-    solLen(sol);
+    solLen(sol, size);
 }
 
-bool isSafe(int maze[10][10], int x, int y, int g_x, int g_y, int sol[10][10], vector<int> distances, int iteration) {
+bool isSafe(int** maze, int x, int y, int g_x, int g_y, int** sol, vector<int> distances, int iteration, size_t size) {
     // cout << "\n Testing coordinates (isSafe): (" << x << ", " << y << ")" << endl;
 
-    if (x >= 0 && x < 10 && y >= 0 && y < 10 && maze[x][y] >= 0 && sol[x][y] != 1 && chebyshevDistance(maze, x, y, g_x, g_y) <= distances[iteration] && maze[x][y] != 2) {
-        // cout << "\n Coordenada valida \n";
+    // Si la posición está dentro de los límites del laberinto, si el mismo espacio en solución no es 1, si la distancia chebyshev entre el punto y la meta es menor o igual a la menor
+    // distancia en el arreglo de distancias y el espacio en el laberinto no es un 2, entonces el espacio es válido.
+    if (x >= 0 && x < size && y >= 0 && y < size && maze[x][y] >= 0 && sol[x][y] != 1 && chebyshevDistance(maze, x, y, g_x, g_y) <= distances[iteration] && maze[x][y] != 2) {
+        // cout << "\n Valid coordinate \n";
         return true;
     }
-    // cout << "\n Coordenada no es valida \n";
+    // cout << "\n Not valid coordinate \n";
     return false;
 }
 
-bool solveMazeUtil(int maze[10][10], int x, int y, int g_x, int g_y, int sol[10][10], int limit, vector<int> distances, bool diagonal, int iteration) {
+bool solveMazeUtil(int** maze, int x, int y, int g_x, int g_y, int** sol, int limit, vector<int> distances, bool diagonal, int iteration, size_t size) {
 
     int i;
     vector<int> distances_the_revenge;
@@ -115,50 +116,58 @@ bool solveMazeUtil(int maze[10][10], int x, int y, int g_x, int g_y, int sol[10]
         return true;
     }
 
-    if (isSafe(maze, x, y, g_x, g_y, sol, distances, 0) == true) {
+    // Detección si el espacio actual es seguro
+    if (isSafe(maze, x, y, g_x, g_y, sol, distances, 0, size) == true) {
         sol[x][y] = 1;
 
         // cout << "\n Distance from actual point to goal: " << chebyshevDistance(maze, x, y, g_x, g_y) << endl;
 
-        cout << "\n Actual solution: " << endl;
-        printSolution(sol);
+        // cout << "\n Actual solution: " << endl;
+        // printSolution(sol, size);
 
-        for (i = 1; i < 10; i++) {
+        for (i = 1; i < size; i++) {
 
+            // Actualizar camino caminable
             if (maze[x][y] == 0) {
                 maze[x][y] = 1;
             }
 
-            distances.push_back(chebyshevDistance(maze, x + i, y, g_x, g_y));
-            distances.push_back(chebyshevDistance(maze, x, y + i, g_x, g_y));
-            distances.push_back(chebyshevDistance(maze, x - i, y, g_x, g_y));
-            distances.push_back(chebyshevDistance(maze, x, y - i, g_x, g_y));
+            // cout << "\n\n Calculating distances..." << endl;
 
+            // Calcular distancias adyacentes
+            if (x + i < size && x + i >= 0)
+                distances.push_back(chebyshevDistance(maze, x + i, y, g_x, g_y));
+            if (y + i < size && y + i >= 0)
+                distances.push_back(chebyshevDistance(maze, x, y + i, g_x, g_y));
+            if (x - i < size && x - i >= 0)
+                distances.push_back(chebyshevDistance(maze, x - i, y, g_x, g_y));
+            if (y - i < size && y - i >= 0)
+                distances.push_back(chebyshevDistance(maze, x, y - i, g_x, g_y));
+
+            // Se insertan las distancias en un arreglo, se ordenan y eliminan duplicados
             distances_the_revenge = insertionSort(distances);
-
             remove(distances_the_revenge);
 
-            /* cout << "\n Distances array after insertionSort: " << endl;
-
+            /*
+            cout << "\n Distances array after insertionSort: " << endl;
             printArray(distances_the_revenge, distances_the_revenge.size());
-
             cout << "\n Distances size: " << distances.size() << endl;
-
             cout << "\n i value: " << i << endl;
+            cout << "\n Best distance: " << distances_the_revenge[0] << endl;
+            */
 
-            cout << "\n Best distance: " << distances_the_revenge[0] << endl; */
-
+            // Recursiones
             if (i <= maze[x][y]) {
-                if (solveMazeUtil(maze, x + i, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration) == true) {
+                if (solveMazeUtil(maze, x + i, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration, size) == true) {
                     return true;
                 }
-                if (solveMazeUtil(maze, x, y + i, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration) == true) {
+                if (solveMazeUtil(maze, x, y + i, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration, size) == true) {
                     return true;
                 }
-                if (solveMazeUtil(maze, x - i, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration) == true) {
+                if (solveMazeUtil(maze, x - i, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration, size) == true) {
                     return true;
                 }
-                if (solveMazeUtil(maze, x, y - i, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration) == true) {
+                if (solveMazeUtil(maze, x, y - i, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration, size) == true) {
                     return true;
                 }
             }
@@ -169,7 +178,7 @@ bool solveMazeUtil(int maze[10][10], int x, int y, int g_x, int g_y, int sol[10]
                 // distances_the_revenge[0] = 100000;
                 maze[x][y] = 2;
                 sol[x][y] = 9;
-                solveMazeUtil(maze, x, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration);
+                solveMazeUtil(maze, x, y, g_x, g_y, sol, limit - 1, distances_the_revenge, diagonal, iteration, size);
             }
         }
 
@@ -179,23 +188,32 @@ bool solveMazeUtil(int maze[10][10], int x, int y, int g_x, int g_y, int sol[10]
     return false;
 }
 
-bool solveMaze(int maze[10][10], int x, int y, int g_x, int g_y, int limit, bool diagonal) {
+bool solveMaze(int** maze, int x, int y, int g_x, int g_y, int limit, bool diagonal, size_t size) {
+    
+    // Lista de distancias
     vector<int> d_list;
 
+    // Se agrega valor grande para comparación
     d_list.push_back(100000);
 
     int iterCheck = 0;
 
     cout << "\n Trying to solve maze: " << endl;
-    printSolution(maze);
+    printSolution(maze, size);
     cout << "\n Start in: (" << x << ", " << y << ")";
     cout << "\n End in: (" << g_x << ", " << g_y << ")" << endl;
     cout << "\n 0 = Walkable space\n 1 = Selected solution\n 2 = Impassable space" << endl;
 
-    int i, j, sol[10][10];
+    int i, j;
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+    // Crear arreglo 2D solución
+    int** sol = new int* [size];
+    for (size_t k = 0; k < size; k++) {
+        sol[k] = new int[size];
+    }
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             if (maze[i][j] > 1) {
                 sol[i][j] = maze[i][j];
                 continue;
@@ -204,19 +222,22 @@ bool solveMaze(int maze[10][10], int x, int y, int g_x, int g_y, int limit, bool
         }
     }
 
-    if (solveMazeUtil(maze, x, y, g_x, g_y, sol, limit, d_list, diagonal, iterCheck) == false) {
+    // Inicio recursión
+    if (solveMazeUtil(maze, x, y, g_x, g_y, sol, limit, d_list, diagonal, iterCheck, size) == false) {
         cout << "\n Solution doesn't exist" << endl;
-        printSolution(sol);
+        printSolution(sol, size);
         return false;
     }
 
     cout << "\n Found solution: " << endl;
-    printSolution(sol);
+    printSolution(sol, size);
     return true;
 }
 
-void readFile(string fileName, int matrix[10][10]) {
+size_t fileSize(string fileName) {
+    // Esto determina el tamaño del laberinto en el archivo entregado
     ifstream mazeFile;
+    string line;
     // string fileName = "mazeFile.txt";
     mazeFile.open(fileName, ios::in);
 
@@ -226,7 +247,28 @@ void readFile(string fileName, int matrix[10][10]) {
         exit(0);
     }
 
-    int SIZE = 10, i = 0, j = 0;
+    while (!mazeFile.eof()) {
+        getline(mazeFile, line);
+    }
+    mazeFile.close();
+
+    size_t liSi = line.size() - (line.size() / 2);
+
+    return liSi;
+}
+
+void readFile(string fileName, int** matrix) {
+    // Esto lee el laberinto en el archivo entregado y lo carga en una matriz
+    ifstream mazeFile;
+    mazeFile.open(fileName, ios::in);
+
+    // Checking error
+    if (mazeFile.fail()) {
+        cout << "Error opening file" << endl;
+        exit(0);
+    }
+
+    size_t SIZE = fileSize(fileName), i = 0, j = 0;
 
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
@@ -236,13 +278,25 @@ void readFile(string fileName, int matrix[10][10]) {
     mazeFile.close();
 }
 
-int main()
-{   
-    int maze[10][10];
+int main(){
+
+    /* MAIN */
+
+    size_t size;
+
+    size = fileSize("mazeFile.txt");
+
+    // Building maze
+    int** maze = new int* [size]; // each element is a pointer to an array.
+    for (size_t i = 0; i < size; ++i) {
+        maze[i] = new int[size];
+    }
+
+    cout << "\n Maze size: " << size << endl;
 
     readFile("mazeFile.txt", maze);
 
-    solveMaze(maze, 9, 2, 0, 9, 20, false);
+    solveMaze(maze, 9, 2, 0, 9, 20, false, size);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
